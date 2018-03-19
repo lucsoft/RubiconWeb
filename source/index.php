65159126d4a796ba22b5ac1feeb467155037de4b
lucsoft <lucsoft@dev107.com>
2018-03-19 15:29:42 +0100

    Added Index
    + index.php

---
+++ source/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <title>RubiconBot - Comming Soon</title>
+ <link href="https://fonts.googleapis.com/css?family=Roboto:300,500|Quicksand:300,400,500,700" rel="stylesheet">
+ <style media="screen">
+    html {
+
+      font-family: 'Roboto';
+      background: rgb(17, 17, 17);
+   }
+   h1, footer {
+      position: fixed;
+      top: 25%;
+      left: 50%;
+      transform: translate(-50%,-50%);
+   }
+   h1 {
+      font-weight: 300;
+      font-size: 3rem;
+   }
+   footer {
+      font-weight: 500;
+      margin-top: 5rem;
+   }
+ </style>
+ </head>
+ <body>
+
+   <h1 style="color: white">RubiconWeb Comming Soon</h1>
+   <footer style="color: white">
+    Copyright (c) 2018 lucsoft All Rights Reserved.
+   </footer>
+ </body>
+</html>
