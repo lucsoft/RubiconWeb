11336634dc7f10eb4330f05e55781f5e72626bf7
lucsoft <lucsoft@dev107.com>
2018-03-19 16:18:17 +0100

    Config Template

---
+++ source/config.php
@@ -0,0 +1,19 @@
+<?php
+$database = array(
+   'name' => '',
+   'username' => '',
+   'password' => '',
+   'host' => '',
+   'name' => '',
+);
+$path = array(
+   'MainCSS' => '',
+   'nav' => '';
+ );
+$config = array(
+   'pathto' => $path,
+   'database' => $database,
+);
+
+return $config;
+ ?>
